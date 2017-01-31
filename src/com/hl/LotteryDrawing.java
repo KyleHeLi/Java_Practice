76275b8bdc9c7d6ffce01ea00d97a4d307d99795
KyleHeLi <hli162@uottawa.ca>
2017-01-31 14:57:24 -0500

    Add a new file LotteryDrawing.java

---
+++ src/com/hl/LotteryDrawing.java
@@ -0,0 +1,54 @@
+package com.hl;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+/**
+* @author Kyle Lee 
+* @version 1.0 - Created date: 2017年1月31日 下午2:44:07
+* @filename Practice/com.hl/LotteryDrawing.java
+* This program demonstrates array mnipulation
+*/
+
+public class LotteryDrawing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How many numbers do you need to draw?");
+		int k = in.nextInt();
+		
+		System.out.println("What is the highest number you can draw?");
+		int n = in.nextInt();
+		
+		// fill an array with numbers 1 2 3 ... n
+		int[] numbers = new int[n];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i + 1;
+		}
+		
+		// draw k numbers and put them into a second array
+		int[] result = new int[k];
+		for (int i = 0; i < result.length; i++) {
+			// make a random index between 0 and n-1
+			int r = (int) (Math.random() * n);
+			
+			// pick the element at teh random location
+			result[i] = numbers[r];
+			
+			// move the last element into the random location
+			numbers[r] = numbers[n - 1];
+			n--;
+		}
+		
+		// print the sorted array
+		Arrays.sort(result);
+		System.out.println("Bet the following combination. It'll make you rich!");
+		for (int r : result)
+			System.out.println(r);
+	}
+
+}
