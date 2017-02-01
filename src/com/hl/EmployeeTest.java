dde14d3e91daeef36b1e58d4a39c9466c5d86fe2
KyleHeLi <hli162@uottawa.ca>
2017-02-01 16:13:23 -0500

    line 23 added initializing Employee() instance with no augments

--- src/com/hl/EmployeeTest.java
+++ src/com/hl/EmployeeTest.java
@@ -15,11 +15,12 @@ public class EmployeeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// fill the staff array with three Employee objects
-		Employee[] staff = new Employee[3];
+		Employee[] staff = new Employee[4];
 		
 		staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
 		staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
 		staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
+		staff[3] = new Employee();
 		
 		// raise everyone's salary by 5%
 		for (Employee e : staff) {
