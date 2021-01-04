d1123a50616fa08f47c25c9cf6eb0008c23bb3c7
Kyle Li <kyleheli888@gmail.com>
2021-01-04 11:01:56 -0500

    added new solutions

---
+++ src/com/leetcode/easy/ValidParentheses_20.java
@@ -0,0 +1,61 @@
+package com.leetcode.easy;
+
+import java.util.Stack;
+
+public class ValidParentheses_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        int length = s.length();
+        if (length % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                if ((c == ')' && stack.peek() == '(') ||
+                        (c == ']' && stack.peek() == '[') ||
+                        (c == '}' && stack.peek() == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses_20 test = new ValidParentheses_20();
+
+        // Example 1
+        String s = "()";
+        System.out.println(test.isValid(s));
+
+        // Example 2
+        s = "()[]{}";
+        System.out.println(test.isValid(s));
+
+        // Example 3
+        s = "(]";
+        System.out.println(test.isValid(s));
+
+        // Example 4
+        s = "([)]";
+        System.out.println(test.isValid(s));
+
+        // Example 5
+        s = "{[]}";
+        System.out.println(test.isValid(s));
+    }
+}
---
+++ src/com/leetcode/medium/MinStack_155.java
@@ -0,0 +1,114 @@
+package com.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinStack_155 {
+
+    private List<Integer> stack;
+    private List<Integer> minStack;
+    private int min;
+    private int minNumber;
+
+    public MinStack_155() {
+        // do intialization if necessary
+        stack = new ArrayList<Integer>();
+        min = Integer.MAX_VALUE;
+        minNumber = 0;
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+        stack.add(number);
+        if (min > number) {
+            min = number;
+            minNumber = 1;
+        } else if (min == number) {
+            minNumber++;
+        }
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        int elemToPop = stack.remove(stack.size() - 1);
+        if (elemToPop == min) {
+            minNumber--;
+            if (minNumber < 1) {
+                if (stack.size() >= 1) {
+                    int tmpMin = stack.get(0).intValue();
+                    for (Integer elem : stack) {
+                        if (elem.intValue() < tmpMin) {
+                            tmpMin = elem.intValue();
+                        }
+                    }
+                    min = tmpMin;
+                } else {
+                    min = Integer.MAX_VALUE;
+                }
+            }
+        }
+        return elemToPop;
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+        return min;
+    }
+
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("== Example1 ==");
+        MinStack_155 test = new MinStack_155();
+        test.push(1);
+        System.out.println(test.min());
+        test.pop();
+        test.push(2);
+        test.push(3);
+        System.out.println(test.min());
+        test.push(1);
+        System.out.println(test.min());
+
+        System.out.println("== Example2 ==");
+        test = new MinStack_155();
+        test.push(1);
+        System.out.println(test.min());
+        test.push(2);
+        System.out.println(test.min());
+        test.push(3);
+        System.out.println(test.min());
+
+        System.out.println("== Example3 ==");
+        test = new MinStack_155();
+        test.push(-1);
+        test.push(-2);
+        System.out.println(test.min());
+        test.pop();
+        test.push(-3);
+        test.push(3);
+        test.push(2);
+        test.pop();
+        test.pop();
+        test.pop();
+        test.pop();
+        test.push(400);
+        test.push(3);
+        test.push(200);
+        test.push(1);
+        System.out.println(test.min());
+        test.pop();
+        System.out.println(test.min());
+    }
+}
